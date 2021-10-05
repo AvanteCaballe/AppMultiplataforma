68f01c5be8ea1ff5cf4b1e5cf5789a104a82799b
AvanteCaballe <marcavante@iesebre.com>
2021-10-05 20:15:09 +0200

    Afegir HelloWorld

---
+++ HelloWorld/app/src/main/java/me/marcavantecaballe/dam/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.marcavantecaballe.dam.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
